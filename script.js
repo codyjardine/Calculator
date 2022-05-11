18bd8ef656d7cae829a55771b2401b063df42002
Cody Jardine <codyjardine@protonmail.com>
2022-05-10 20:14:39 -0400

    Cleaned up file

--- script.js
+++ script.js
@@ -20,17 +20,25 @@ function calculate(operand1, operator, operand2) {
   if (operator === '+') {
     return add(operand1, operand2);
   }
+
   if (operator === '-') {
     return subtract(operand1, operand2);
   }
+
   if (operator === '*') {
     return multiply(operand1, operand2);
   }
+
   if (operator === '/') {
     let divideResult = divide(operand1, operand2);
     let divideResultMax = divideResult.toString().length;
-    if (divideResult % 1 === divideResult) return divideResult;
-    if (divideResultMax > 6) return divideResult.toFixed(6);
+
+    if (divideResult % 1 === divideResult) {
+      return divideResult;
+    }
+    if (divideResultMax > 6) {
+      return divideResult.toFixed(6);
+    }
     return divideResult;
   }
 }
@@ -47,10 +55,13 @@ function operator(e) {
 
 function onDisplay(e) {
   let firstNumberCount = firstNumber.toString().length;
-  if (firstNumberCount > 13) return;
+  if (firstNumberCount > 13) {
+    return;
+  }
+
   if (e.target) {
     firstNumber += e.target.getAttribute('data-value');
-   return display.textContent = firstNumber;
+    return display.textContent = firstNumber;
   }
   firstNumber += e.getAttribute('data-value');
   display.textContent = firstNumber;
@@ -61,19 +72,19 @@ function reset() {
   operation = '';
 }
 
-// computes the whole arithmethic operation and other '=' behaviours of calculator
 function equals() {
   if (firstNumber === '' || operation === '') {
     reset();
     return (display.textContent = `${Number(firstNumber)}`);
   }
   firstNumber = Number(firstNumber);
-  // divison by zero
+
   if (firstNumber === 0 && secondNumber !== 0) {
     return (display.textContent = 'DIV BY ZERO');
   }
   let output = calculate(secondNumber, operation, firstNumber);
   let outputCount = output.toString().length;
+
   if (outputCount > 13) {
     output = Number(output.toString().slice(0, 13));
   }
@@ -82,7 +93,6 @@ function equals() {
   reset();
 }
 
-// default
 let display = document.getElementById('displayContent');
 display.textContent = '0';
 
@@ -92,7 +102,6 @@ clearEverything.addEventListener('click', () => {
   firstNumber = '';
 });
 
-// backspace funtionality
 function removeLast() {
   firstNumberCount = firstNumber.toString().length;
   if (firstNumberCount === 1 || firstNumber === '') {
@@ -130,23 +139,19 @@ function decimalFunction() {
 let decimalButton = document.querySelector(`button[data-value='.']`);
 decimalButton.addEventListener('click', decimalFunction);
 
-// equals button
 let equalsButton = document.querySelector(`button[data-value='Enter']`);
 equalsButton.addEventListener('click', equals);
 
-// All operator buttons 
 let operatorButtons = document.querySelectorAll(`button[data-type='operator']`);
 operatorButtons.forEach((operatorButton) => {
   operatorButton.addEventListener('click', operator);
 });
 
-// All number buttons
 let numbers = document.querySelectorAll(`button[data-type='number']`);
 numbers.forEach((number) => {
   number.addEventListener('click', onDisplay);
 });
 
-// keyboard event listeners 
 function keyboardDigits(e) {
   let key = document.querySelector(`button[data-value='${e.key}']`);
   if (e.key === '/') e.preventDefault();
